1b4ed8a37297d67cabe5a41f7658d9853e0ef37b
amorilla42 <andresmm4242@gmail.com>
2024-07-11 14:16:45 +0200

    feat(module01): ex03 done

--- C01/ex03/HumanB.cpp
+++ C01/ex03/HumanB.cpp
@@ -1,6 +1,8 @@
 #include "HumanB.hpp"
 
-HumanB::HumanB(std::string name) : name(name) {}
+HumanB::HumanB(std::string name) : name(name) {
+    this->weapon = NULL;
+}
 
 HumanB::~HumanB() {}
 
--- C01/ex03/main.cpp
+++ C01/ex03/main.cpp
@@ -3,20 +3,17 @@
 #include "HumanB.hpp"
 
 int main() {
-    Weapon weapon = Weapon("MANTECA");
-    HumanA juan = HumanA("JUAN", weapon);
-    juan.attack();
-    weapon.setType("MIERDONAZO");
-    juan.attack();
+    Weapon weapon = Weapon("Stick");
+    HumanA humanA = HumanA("ELPEPE", weapon);
+    humanA.attack();
+    weapon.setType("Water");
+    humanA.attack();
 
-    Weapon weapon2 = Weapon("CACA");
-    std::cout << "Weapon2 type: " << weapon2.getType() << std::endl;
-    weapon2.setType("WEONCLUIAO");
-    std::cout << "Weapon2 type: " << weapon2.getType() << std::endl;
-    HumanB pedro = HumanB("ETTESECH");
-    pedro.attack();
-    pedro.setWeapon(weapon2);
-    pedro.attack();
+    Weapon weapon2 = Weapon("Soviet Heavy Tank KV-2 (Kliment Voroshilov 2) using high explosive steel fragmented ammunition model OF-530 weighing 48 kg");
+    HumanB humanB = HumanB("ETTESECH");
+    humanB.attack();
+    humanB.setWeapon(weapon2);
+    humanB.attack();
 
     return 0;
 }
