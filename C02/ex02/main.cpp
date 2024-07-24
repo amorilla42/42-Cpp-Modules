ed90365c1fa1dfd5397d218ccf4ed43fc233ce83
amorilla <andresmm4242@gmail.com>
2024-07-24 12:56:55 +0200

    fix(module02): ex02 fixed console output

--- C02/ex02/main.cpp
+++ C02/ex02/main.cpp
@@ -29,7 +29,7 @@ int main( void ) {
     std::cout << a << std::endl;
     std::cout << a++ << std::endl<< std::endl;
 
-    std::cout << Fixed::max( a, b ) << std::endl<< std::endl;
+    std::cout << Fixed::max( a, b ) << std::endl;
     std::cout << Fixed::min( a, b ) << std::endl;
     return 0;
 }
